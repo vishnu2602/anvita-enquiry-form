ebe975541ebe4a393ecd119f31a225f9141317bd
vishnu2602 <vishnu2602@gmail.com>
2015-07-30 10:45:15 +0530

    Revert "bootstrap functionality removed"
    This reverts commit 42471136e33c3447b2e6e07a81d7a08ea3a9a64c.

--- templates/basic.php
+++ templates/basic.php
@@ -1,30 +1,32 @@
 <div class="anvita-enq basic">
 	<form method='post' role='form' name='quickcontact' id='quickcontact'>
-		<div style="width:100%;" class="enq_msg"></div>
+		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 enq_msg"></div>
 		<input type="hidden" class="enq-type" name="enq-type" value="normal"/>
 		<input type='text' name='enq-name' id='enqs-name' class='form-control enq-name enq-input' placeholder='Name'/>
 		<input type='text' name='enq-email' id='enqs-email' class='form-control enq-input enq-email' placeholder='Email'/>
-		<select name='enq-country' class="enq-country anv_select_country form-control enq-input select" id='enq-country' onChange='getSelectedCountry(this)'>
+		<select name='enq-country' class="enq-country anv_select_country form-control enq-input select" id='' onChange='getSelectedCountry(this)'>
 			<option disabled selected>Select Country</option>
 		</select>
 		<input type='hidden' class='enq-selectedCountry' name='enq-selectedCountry' id='enq-selectedCountry'/>
 		<input type='text' name='enq-city' id='enqs-city' class='form-control enq-input enq-city' placeholder='City'/>
-		<div class="mobile_box" style="float:left; width:100%;">
-			<div style="width:20%; float:left;"><input name='phonecode' id='phonecode' type='text' class='form-control enq-newval enq-input phonecode'></div>	
-			<div style="width:79%; float:left;"><input style="margin-left:4px;" name='phone' id='phone1' type='text' class='quickenqfield form-control mobi enq-newval enq-input enq-phone' placeholder='Phone'/></div>
-		</div>	
-		<div class="mobile_box" style="float:left; width:100%;">
-			<div style="width:20%; float:left;"><input type='text' class='form-control land enq-input enq-newval phonecode2' id='phonecode2' name='phonecode2' /></div>
-			<div style="width:29%; float:left;"><input style="margin-left:4px;" type='text' id='areacode' class='form-control land enq-newval enq-input enq-area' name='areacode' placeholder='Area Code'/></div>
-			<div style="width:50%; float:left;"><input style="margin-left:6px;" type='text' id='phone2' class='form-control land enq-newval enq-input enq-phone2' name='phone2' placeholder='Phone'/></div>
+		<div class="mobile_box">
+		<div class='row'>
+			<div class='col-xs-3'><input name='phonecode' id='phonecode' type='text' class='form-control enq-newval enq-input phonecode'></div>	
+			<div class='col-xs-9'><input name='phone' id='phone1' type='text' class='quickenqfield form-control mobi enq-newval enq-input enq-phone' placeholder='Phone'/></div>
+		</div>
+		<div class='row'>
+			<div class='col-xs-3'><input type='text' class='form-control land enq-input enq-newval phonecode2' id='phonecode2' name='phonecode2' /></div>
+			<div class='col-xs-3'><input type='text' id='areacode' class='form-control land enq-newval enq-input enq-area' name='areacode' placeholder='Area Code'/></div>
+			<div class='col-xs-6'><input type='text' id='phone2' class='form-control land enq-newval enq-input enq-phone2' name='phone2' placeholder='Phone'/></div>
+		</div>
 		</div>
 		<textarea Placeholder='Message' class='form-control enq-input enq-address' name='enq-msg' id='enq-msg'></textarea>
-		<div class="captcha">
+		<div class="row captcha">
 		<?php $enq_rand='enq-'.rand(0,999); ?>
 		<input type="hidden" class="enq-captchavar" name="enq-var" value="<?php echo $enq_rand; ?>"/>
-			<div style="width:40%; float:left;"><input type="text" class="enq-captcha enq-input form-control" placeholder="Captcha" name="enq-captcha"/></div>
-			<div style="width:60%; float:left;"><img style="width:90px;" class="enq-captchaimg" src="<?php echo ANVITA_ENQUIRY_PLUGIN_URL.'captcha.php?var='.$enq_rand; ?>"/>
-			<img class="enq-refresh" src="<?php echo ANVITA_ENQUIRY_PLUGIN_URL; ?>refresh.png"/>
+			<div class='col-xs-6'><input type="text" class="enq-captcha enq-input form-control" placeholder="Captcha" name="enq-captcha"/></div>
+			<div class='col-xs-6'><img class="enq-captchaimg" src="<?php echo ANVITA_ENQUIRY_PLUGIN_URL.'captcha.php?var='.$enq_rand; ?>"/>
+				<br/><img class="enq-refresh" src="<?php echo ANVITA_ENQUIRY_PLUGIN_URL; ?>refresh.png"/>
 			</div>
 		</div>
 		<div class="enq-msg"></div>
