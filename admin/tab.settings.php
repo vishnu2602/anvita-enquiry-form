f65026c0655eef3df9394fcb75dc084ee23728bf
aswanthkumarks <aswanthkumar25@gmail.com>
2015-08-04 16:35:54 +0530

    Bug Fix

--- admin/tab.settings.php
+++ admin/tab.settings.php
@@ -1,17 +1,27 @@
-<pre><?
-print_r($settings);
-?></pre>
+<?php 
+$to="";
+$cc="";
+$bcc="";
+$crm="";
+$ph="";
+if(isset($settings['email']['to'])) $to=$settings['email']['to'];
+if(isset($settings['email']['cc'])) $cc=$settings['email']['cc'];
+if(isset($settings['email']['bcc'])) $bcc=$settings['email']['bcc'];
+if(isset($settings['crm'])) $crm=$settings['crm'];
+if(isset($settings['phone'])) $ph=$settings['phone'];
+
+?>
 <div class="form-div wrap">
 	<div class="timemsg"></div>
 	<table class="form-table">
 		<tbody>
-			<tr><th scope="row"><label>Email Alert To:</label> </th><td><input type="text" class="regular-text code" id="eto" name="enq-setting[email]" value="<?php echo $settings['email']['to']; ?>"/>
+			<tr><th scope="row"><label>Email Alert To:</label> </th><td><input type="text" class="regular-text code" id="eto" name="enq-setting[email]" value="<?php echo $to; ?>"/>
 				<p class="description">seperate emails with comma (,)</p>
 			</td></tr>
-			<tr><th scope="row">CC: </th><td><input type="text" class="regular-text code" id="ecc" name="enq-setting[cc]" value="<?php echo $settings['email']['cc']; ?>"/> </td></tr>
-			<tr><th scope="row">BCC: </th><td><input type="text" class="regular-text code" id="ebcc" name="enq-setting[bcc]" value="<?php echo $settings['email']['bcc']; ?>"/> </td></tr>
-			<tr><th scope="row">CRM ID: </th><td><input type="text" class="regular-text code" id="enq_crm" name="anvita-crmid" value="<?php echo $settings['crm'];?>"/> </td></tr>
-			<tr><th scope="row">Phone: </th><td><input type="text" class="regular-text code" id="phn" name="phne" value="<?php echo $settings['phone'];?>"/> </td></tr>
+			<tr><th scope="row">CC: </th><td><input type="text" class="regular-text code" id="ecc" name="enq-setting[cc]" value="<?php echo $cc; ?>"/> </td></tr>
+			<tr><th scope="row">BCC: </th><td><input type="text" class="regular-text code" id="ebcc" name="enq-setting[bcc]" value="<?php echo $bcc; ?>"/> </td></tr>
+			<tr><th scope="row">CRM ID: </th><td><input type="text" class="regular-text code" id="enq_crm" name="anvita-crmid" value="<?php echo $crm;?>"/> </td></tr>
+			<tr><th scope="row">Phone: </th><td><input type="text" class="regular-text code" id="phn" name="phne" value="<?php echo $ph;?>"/> </td></tr>
 		</tbody>
 	</table>
 	<p class="submit"><input type="button" name="submit" id="enq-settings1" class="button button-primary" value="Save Changes"></p>
